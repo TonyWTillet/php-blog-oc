49312b3de2c6382e13b0fa4a6811a1fd055fc510
TonyTillet <tony@wagaia.com>
2022-12-15 22:29:17 +0100

    Setup next step

--- src/Commands/Edit/EditCommands.php
+++ src/Commands/Edit/EditCommands.php
@@ -4,5 +4,15 @@ namespace App\Commands\Edit;
 
 class EditCommands
 {
+    private string $table;
 
+    public function __construct(string $table)
+    {
+        $this->table = $table;
+    }
+
+    public function save(): void
+    {
+        $this->save();
+    }
 }

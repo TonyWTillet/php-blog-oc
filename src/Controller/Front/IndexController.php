362ee9846d39156b4cbb39d10b285dc7aa828cf6
tonyttillet <tony@wagaia.com>
2023-01-26 20:31:49 +0100

    BLG-004: Queries Variables for home view

--- src/Controller/Front/IndexController.php
+++ src/Controller/Front/IndexController.php
@@ -4,17 +4,20 @@ namespace App\Controller\Front;
 
 use App\Queries\CategoryQueries;
 use App\Queries\GlobalsQueries;
+use App\Queries\PostQueries;
 use Exception;
 
 class IndexController extends FrontController
 {
     private GlobalsQueries $globalsQueries;
     private CategoryQueries $categoryQueries;
+    private PostQueries $postQueries;
 
     public function __construct()
     {
         $this->globalsQueries = new GlobalsQueries();
         $this->categoryQueries = new CategoryQueries();
+        $this->postQueries = new PostQueries();
     }
 
     /**
@@ -23,8 +26,11 @@ class IndexController extends FrontController
     public function index()
     {
         $categories = $this->categoryQueries->getCategories();
+        $globals = $this->globalsQueries->getGlobals();
+        $posts = $this->postQueries->getPosts();
         $data['categories'] = $categories;
         $data['globals'] = $globals;
+        $data['posts'] = $posts;
         require $this->Twig('home', $data, 'data');
     }
 }

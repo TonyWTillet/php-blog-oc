642fdd87eb9493940ac72a0c116d4f4ca13d6278
tonyttillet <tony@wagaia.com>
2023-03-14 12:36:56 +0100

    BLG-021 :Globals modified / save method.

--- src/Controller/Back/GlobalsController.php
+++ src/Controller/Back/GlobalsController.php
@@ -3,24 +3,28 @@
 namespace App\Controller\Back;
 
 
+use App\Commands\Edit\GlobalsEditCommands;
 use App\Controller\RequireAuhtentification;
 use App\Queries\GlobalsQueries;
 
 class GlobalsController extends BackController implements RequireAuhtentification
 {
     private GlobalsQueries $globalsQueries;
+    private GlobalsEditCommands $globalsCommands;
 
     public function __construct()
     {
         $this->globalsQueries = new GlobalsQueries();
+        $this->globalsCommands = new GlobalsEditCommands();
     }
 
     public function index()
     {
 
         $globals  = $this->globalsQueries->getGlobals();
-
-
+        if ($_POST) {
+            $modif = $this->globalsCommands->save($_POST['name']);
+        }
         require $this->Twig('globals', $globals, 'globals');
     }
 }

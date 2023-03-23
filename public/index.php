8294afea0a972b98387e869e874c503ee2e7ada5
tonyttillet <tony@wagaia.com>
2023-03-23 14:50:48 +0100

    MASTER : refactoring

--- public/index.php
+++ public/index.php
@@ -3,11 +3,9 @@ session_start();
 error_reporting(0);
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-use App\Controller\HomeController;
+use App\Service\RoutingService;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'../../');
 $dotenv->load();
 
-$homeController = new HomeController();
-
-$homeController->index();
+RoutingService::route();
--- src/Service/RoutingService.php
+++ src/Service/RoutingService.php
@@ -1,26 +1,25 @@
 <?php
 
-namespace App\Controller;
-
-use App\Service\AuthentificationService;
+namespace App\Service;
 
 
-class HomeController
+class RoutingService
 {
     /**
      * The index function of the FrontController. It is the first function called when the user access the website.
      */
-    public function index() {
+    public static function route(): void
+    {
 
         $link = $_SERVER['REQUEST_URI'];
-        $link_array = explode('/',$link);
+        $link_array = explode('/', $link);
         $redirect = $link_array[1];
 
         if ($redirect == 'panel') {
             $url = explode('/', $_SERVER['REQUEST_URI']);
             $page = $url[2];
             if (!empty($url[3])) {
-                $action = explode('?',$url[3])[0];
+                $action = explode('?', $url[3])[0];
             }
             if (empty($url[3])) {
                 $action = 'index';
@@ -29,7 +28,7 @@ class HomeController
                 $page = 'login';
                 $action = 'logger';
             }
-            if (empty($url[3]) &&  $url[2] == 'login') {
+            if (empty($url[3]) && $url[2] == 'login') {
                 $action = 'login';
             }
 
@@ -50,13 +49,11 @@ class HomeController
             }
 
             $controller->$action();
-        }
-
-        else {
+        } else {
             $url = explode('/', $_SERVER['REQUEST_URI']);
             $page = $url[1];
             if (!empty($url[2])) {
-                $action = explode('?',$url[2])[0];
+                $action = explode('?', $url[2])[0];
             }
             if (empty($url[1])) {
                 $page = 'index';

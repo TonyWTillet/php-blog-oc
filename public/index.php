6569af31b5844098cdfb76d24e62d5b691f09937
tonyttillet <tony@wagaia.com>
2023-03-31 13:21:57 +0200

    MASTER : refactoring

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-error_reporting(0);
+error_reporting(1);
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use App\Service\RoutingService;
--- src/Middleware/CommentRepository.php
+++ src/Middleware/CommentRepository.php
@@ -23,7 +23,7 @@ class CommentRepository extends Database
     public function findAllComments(): array
     {
         try {
-            $req = $this->getPDO()->prepare("SELECT a.*, b.id as articlesId, c.id as usersId, c.user_name , b.post_title FROM $this->table a LEFT JOIN blog_posts b ON b.id = a.post_id LEFT JOIN blog_users c ON a.user_id = c.id WHERE a.flag = 0 ORDER BY a.created_at DESC");
+            $req = $this->getPDO()->prepare("SELECT a.*, b.id as articlesId, c.id as usersId, c.user_name , b.post_title FROM $this->table a LEFT JOIN blog_posts b ON b.id = a.post_id LEFT JOIN blog_users c ON a.user_id = c.id WHERE a.flag = 0 AND b.post_title IS NOT NULL ORDER BY a.created_at DESC");
             $req->execute(array());
             $comments=$req->fetchAll(PDO::FETCH_CLASS, Comment::class);
             if (!$comments) {
@@ -40,7 +40,7 @@ class CommentRepository extends Database
     public function findAllCommentsByPostId(int $postId): array
     {
         try {
-            $req = $this->getPDO()->prepare("SELECT a.*, b.id as articlesId, c.id as usersId, c.user_name , b.post_title FROM $this->table a LEFT JOIN blog_posts b ON b.id = a.post_id LEFT JOIN blog_users c ON a.user_id = c.id WHERE a.flag = 0 AND a.post_id = ? ORDER BY a.created_at DESC");
+            $req = $this->getPDO()->prepare("SELECT a.*, b.id as articlesId, c.id as usersId, c.user_name , b.post_title FROM $this->table a LEFT JOIN blog_posts b ON b.id = a.post_id LEFT JOIN blog_users c ON a.user_id = c.id WHERE a.flag = 0 AND a.post_id = ? AND b.post_title IS NOT NULL ORDER BY a.created_at DESC");
             $req->execute(array($postId));
             $comments=$req->fetchAll(PDO::FETCH_CLASS, Comment::class);
             if (!$comments) {
